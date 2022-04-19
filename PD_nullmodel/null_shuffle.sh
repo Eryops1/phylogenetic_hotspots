c8ae814fed67792ebc85e6ba418216df211a8e4e
Eryops1 <tietje.melanie@gmail.com>
2022-04-19 16:40:53 +0200

    fix runtime in bashscript

--- PD_nullmodel/null_shuffle.sh
+++ PD_nullmodel/null_shuffle.sh
@@ -7,7 +7,7 @@
 #SBATCH --partition normal
 #SBATCH --mem-per-cpu=8gb
 #SBATCH --cpus-per-task 1
-#SBATCH --time 00:15:00
+#SBATCH --time 00:30:00
 #SBATCH --output=/dev/null
 
 source ~/miniconda3/bin/activate R-env-4
