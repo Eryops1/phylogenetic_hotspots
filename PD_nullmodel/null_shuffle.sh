3f1dcec71d5de6e46ab4fb8a0b4ccac0652ee4dc
Eryops1 <tietje.melanie@gmail.com>
2022-04-20 09:20:24 +0200

    increased runtime for colwise nullmodel shuffle

--- PD_nullmodel/null_shuffle.sh
+++ PD_nullmodel/null_shuffle.sh
@@ -7,7 +7,7 @@
 #SBATCH --partition normal
 #SBATCH --mem-per-cpu=8gb
 #SBATCH --cpus-per-task 1
-#SBATCH --time 00:30:00
+#SBATCH --time 00:60:00
 #SBATCH --output=/dev/null
 
 source ~/miniconda3/bin/activate R-env-4
