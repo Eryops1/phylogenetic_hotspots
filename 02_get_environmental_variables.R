c2e7624683dcc4628baaedc025b9ac7dcedaee1f
Eryops1 <tietje.melanie@gmail.com>
2022-05-19 18:44:10 +0200

    more varaiblaes

--- 02_get_environmental_variables.R
+++ 02_get_environmental_variables.R
@@ -293,7 +293,8 @@ shp <- merge(shp, global_drivers, by.x="LEVEL3_COD", by.y="LEVEL_3_CO", all.x=TR
 
 ## Load Environment data ---------------------------------------------------
 
-vars <- c('hfp', 'deforest', 'bio1', 'bio5', 'bio6', 'bio7', 'bio12')
+vars <- c('hfp','deforest','bio1','bio5','bio6','bio7','bio12','bio15','PC_primf','PC_primn','PC_secdf','PC_secdn','PC_urban','PC_c3ann','PC_c4ann','PC_c3per','PC_c4per','PC_c3nfx','PC_pastr','PC_range','PC_secmb','PC_secma','FC_primf','FC_primn','FC_secdf','FC_secdn','FC_urban','FC_c3ann','FC_c4ann','FC_c3per','FC_c4per','FC_c3nfx','FC_pastr','FC_range','FC_secmb','FC_secma')
+
 var.list <- lapply(paste0("environment/", vars, ".rds"), readRDS)
 names(var.list) <- vars
 env.df <- do.call(cbind.data.frame, var.list)
