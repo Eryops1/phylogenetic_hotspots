8e2c2ab0d552ccedef03301c1a4e56bdbf6a1c75
Eryops1 <tietje.melanie@gmail.com>
2022-05-19 18:39:33 +0200

    bootstrap for 100 phylos scripts

---
+++ PD_nullmodel/bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --account PDiv
+#SBATCH --job-name=subsample
+#SBATCH --mail-type=FAIL,END
+#SBATCH --mail-user=melanie.tietje@bio.au.dk
+#SBATCH --partition normal
+#SBATCH --mem-per-cpu=8gb
+#SBATCH --cpus-per-task 1
+#SBATCH --time 00:20:00
+#SBATCH --output=/dev/null
+
+source ~/miniconda3/bin/activate R-env-4
+Rscript manual_bootstrap.R $this_rep 
---
+++ PD_nullmodel/manual_bootstrap.R
@@ -0,0 +1,37 @@
+
+# get rep number (=tree number)
+args <- commandArgs()
+print(args)
+rep <- as.numeric(args[6])
+
+
+library(phyloregion) # PD calculations etc
+library(phytools)
+load('comm_and_phy.RData')
+submat <- submat[[rep]]
+subphy <- subphy[[rep]]
+
+res <- matrix(ncol=1, nrow=nrow(submat)) 
+res.sd <- matrix(ncol=1, nrow=nrow(submat)) 
+for(i in 1:nrow(submat)){ #
+  species.level <- colnames(submat)[which(submat[i,]==1)]
+  physub <- keep.tip(subphy, species.level)
+  tmp <- for(j in 1:1000){ 
+    if(length(species.level)<45){
+      tmp <- NA
+      tmp
+    }else{
+      specs <- sample(species.level, 45)
+      physub2 <- keep.tip(physub, specs)
+      tmp <- sum(physub2$edge.length) # PD sensu stricto
+      tmp
+      j <- j+1
+    }
+  }
+  res[i,1] <- mean(unlist(tmp), na.rm=T)
+  res.sd[i,1] <- sd(unlist(tmp), na.rm=T)
+  if(!i%%1)cat(i,"\r")
+}
+  
+  
+  saveRDS(PD_ses_tipshuffle, file=paste0('bootstrap_', rep, '.rds'))
