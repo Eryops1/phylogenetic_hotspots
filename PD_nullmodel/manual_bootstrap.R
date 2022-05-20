47980c0f1a8d9ba940083f9151b6564aa307bcc6
Eryops1 <melanie.tietje@bios.au.dk>
2022-05-20 16:15:03 +0200

    new results

--- PD_nullmodel/manual_bootstrap.R
+++ PD_nullmodel/manual_bootstrap.R
@@ -17,20 +17,20 @@ res.sd <- matrix(ncol=1, nrow=nrow(submat))
 for(i in 1:nrow(submat)){ #
   species.level <- colnames(submat)[which(submat[i,]==1)]
   physub <- keep.tip(subphy, species.level)
-  tmp <- for(j in 1:1000){ 
+  tmp <- c()
+  for(j in 1:1000){ 
     if(length(species.level)<45){
       tmp <- NA
       tmp
     }else{
       specs <- sample(species.level, 45)
       physub2 <- get_subtree_with_tips(physub, specs) # is this the way to go? what about all other 
-      tmp <- sum(physub2$subtree$edge.length) # PD sensu stricto
-      tmp
+      tmp <- c(tmp, sum(physub2$subtree$edge.length)) # PD sensu stricto
       j <- j+1
     }
   }
-  res[i,1] <- mean(unlist(tmp), na.rm=T)
-  res.sd[i,1] <- sd(unlist(tmp), na.rm=T)
+  res[i,1] <- mean(tmp, na.rm=T)
+  res.sd[i,1] <- sd(tmp, na.rm=T)
   if(!i%%1)cat(i,"\r")
 }
   
