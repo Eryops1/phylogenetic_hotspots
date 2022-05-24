0571433986b7bd0045f2427c8893bd3144bda85a
Eryops1 <melanie.tietje@bios.au.dk>
2022-05-24 14:47:04 +0200

    added .out to gitignore, new run AvTD

--- PD_nullmodel/manual_bootstrap.R
+++ PD_nullmodel/manual_bootstrap.R
@@ -16,23 +16,34 @@ res <- matrix(ncol=1, nrow=nrow(submat))
 res.sd <- matrix(ncol=1, nrow=nrow(submat)) 
 for(i in 1:nrow(submat)){ #
   species.level <- colnames(submat)[which(submat[i,]==1)]
-  physub <- keep.tip(subphy, species.level)
+  physub <- get_subtree_with_tips(subphy, species.level)
   tmp <- c()
-  for(j in 1:1000){ 
-    if(length(species.level)<45){
-      tmp <- NA
-      tmp
-    }else{
-      specs <- sample(species.level, 45)
-      physub2 <- get_subtree_with_tips(physub, specs) # is this the way to go? what about all other 
-      tmp <- c(tmp, sum(physub2$subtree$edge.length)) # PD sensu stricto
-      j <- j+1
-    }
-  }
-  res[i,1] <- mean(tmp, na.rm=T)
-  res.sd[i,1] <- sd(tmp, na.rm=T)
+  # # PD sensu stricto
+  # for(j in 1:1000){ 
+  #   if(length(species.level)<45){
+  #     tmp <- NA
+  #     tmp
+  #   }else{
+  #     specs <- sample(species.level, 45)
+  #     physub2 <- get_subtree_with_tips(physub$subtree, specs) # is this the way to go? what about all other 
+  #     tmp <- c(tmp, sum(physub2$subtree$edge.length)) # PD sensu stricto
+  #     j <- j+1
+  #   }
+  # }
+  #
+  # # Mean pairwise distances
+  # calculate distances between all internal nodes
+  Ntips <- Ntip(physub$subtree)
+  only_clades = c(1:Ntips)
+  distances = get_all_pairwise_distances(physub$subtree, only_clades)
+  res <- c(res, mean(as.numeric(distances)))
+  res.sd <- c(res.sd, sd(as.numeric(distances)))
+  
+  # res[i,1] <- mean(tmp, na.rm=T)
+  # res.sd[i,1] <- sd(tmp, na.rm=T)
   if(!i%%1)cat(i,"\r")
 }
   
 res.all <- list(res, res.sd)  
-saveRDS(res.all, file=paste0('bootstrap_', rep, '.rds'))
+# saveRDS(res.all, file=paste0('bootstrap_', rep, '.rds'))
+saveRDS(res.all, file=paste0('pairwise_dist_', rep, '.rds'))
--- PD_nullmodel/null_shuffle.sh
+++ PD_nullmodel/null_shuffle.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 #SBATCH --account PDiv
-#SBATCH --job-name=nullshuffle
+#SBATCH --job-name=all_indices
 #SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=melanie.tietje@bio.au.dk
 #SBATCH --partition normal
-#SBATCH --mem-per-cpu=8gb
+#SBATCH --mem-per-cpu=16gb
 #SBATCH --cpus-per-task 1
-#SBATCH --time 00:30:00
-#SBATCH --output=nullshuffle.out
+#SBATCH --time 14:00:00
+#SBATCH --output=nullshuffle_indices.out
 
 source ~/miniconda3/bin/activate R-env-4
-Rscript null_shuffle.R $this_rep $this_model >logfile.txt
+Rscript null_shuffle.R $this_rep $this_model >logfile_14h.txt
