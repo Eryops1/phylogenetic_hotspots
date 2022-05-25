8c58d1f2f484fb7a55228862f99f9922bbf41c8f
Eryops1 <melanie.tietje@bios.au.dk>
2022-05-25 08:03:14 +0200

    Merge branch 'main' of github.com:Eryops1/phylogenetic_hotspots into main
    ctly

--- PD_nullmodel/null_shuffle.R
+++ PD_nullmodel/null_shuffle.R
@@ -19,30 +19,16 @@ print("PD_ses done")
 # AvTD
 tmp <- sparse2dense(submat[[rep]])
 tmp <- as.data.frame(tmp)
-res.avTD <- ecospat.calculate.pd(subphy[[rep]], tmp, type="AvTD")
+res.avTD <- ecospat.calculate.pd(subphy[[rep]], tmp, type="AvTD", method="pairwise")
 #saveRDS(res.avTD, file=paste0("AvTD_", rep, '.rds'))
 
 print("AvTD done")
 
 # TTD
-res.TTD <- ecospat.calculate.pd(subphy[[rep]], tmp, type="TTD")
+res.TTD <- ecospat.calculate.pd(subphy[[rep]], tmp, type="TTD", method="pairwise")
 #saveRDS(res.TTD, file=paste0("TTD_", rep, '.rds'))
 
 print("TTD done")
 
 all <- list(PD_ses_tipshuffle, res.avTD, res.TTD)
 saveRDS(all, file=paste0("indices_", rep, '.rds'))
-
-
-
-
-sp <- sample(subphy[[1]]$tip.label, 40000)
-sphy <- keep.tip(subphy[[1]], sp)
-tmp <- sparse2dense(submat[[1]])
-tmp <- as.data.frame(tmp)
-tmp <- tmp[,names(tmp) %in% sp]
-
-system.time({
-  pd <- ecospat.calculate.pd(sphy, tmp, method = "TTD")
-})
-
