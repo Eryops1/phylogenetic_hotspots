26dec8bbb6dc3ce0780d4890c3462c6fe2618c7e
Eryops1 <melanie.tietje@bios.au.dk>
2022-07-12 09:26:03 +0200

    fixing bash scrpt git edits

--- sesPD_bash.sh
+++ sesPD_bash.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 #SBATCH --account PDiv
-<<<<<<< HEAD
 #SBATCH --job-name=picante
-=======
 #SBATCH --job-name=picante SES_PD
->>>>>>> eb3e0b88f1189ea00a5abcc0d9462d2527741ad8
 #SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=melanie.tietje@bio.au.dk
 #SBATCH --partition normal
