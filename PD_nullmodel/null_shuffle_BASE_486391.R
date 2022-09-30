27baed2610df935cde2a4ba3f503d54918850325
Eryops1 <tietje.melanie@gmail.com>
2022-09-30 12:24:46 +0200

    added backup files from merging

---
+++ PD_nullmodel/null_shuffle_BASE_486391.R
@@ -0,0 +1,37 @@
+
+# get rep number (=tree number)
+args <- commandArgs()
+print(args)
+rep <- as.numeric(args[6])
+model <- args[7]
+
+library(phyloregion) # PD calculations etc
+library(ecospat)
+load('comm_and_phy.RData')
+
+# PD_ses
+# PD_ses_tipshuffle <- PD_ses(submat[[rep]], subphy[[rep]], model=model, reps=1000)
+#PE_tipshuffle <- phylo_endemism((submat[[rep]], subphy[[rep]])
+#saveRDS(PD_ses_tipshuffle, file=paste0(model,'_', rep, '.rds'))
+
+print("PD_ses done")
+
+# AvTD
+tmp <- sparse2dense(submat[[rep]])
+tmp <- as.data.frame(tmp)
+rm(submat)
+sphy <- subphy[[rep]]
+rm(subphy)
+res.avTD <- ecospat.calculate.pd(sphy, tmp, type="AvTD", method="pairwise")
+saveRDS(res.avTD, file=paste0("AvTD_", rep, '.rds'))
+
+print("AvTD done")
+
+# TTD
+res.TTD <- ecospat.calculate.pd(sphy, tmp, type="TTD", method="pairwise")
+#saveRDS(res.TTD, file=paste0("TTD_", rep, '.rds'))
+
+print("TTD done")
+
+all <- list(res.avTD, res.TTD) #PD_ses_tipshuffle, 
+saveRDS(all, file=paste0("indices_", rep, '.rds'))
